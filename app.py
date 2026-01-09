8b00292fae9205b253e9adbcc2f0bc5896a191cb
“Sanket <you@example.com>
2026-01-09 20:33:07 +0530

    fixed the bug for no routes found afer valid selection

--- app.py
+++ app.py
@@ -4,24 +4,24 @@ import os
 
 app = Flask(__name__)
 
-# --- SETUP ---
+# SETUP
 DATA_FILE = os.path.join("data", "metro_routes.json")
 if os.path.exists(DATA_FILE):
     graph = MetroGraph.from_json(DATA_FILE)
 else:
     graph = MetroGraph()
 
-# --- ROUTE 1: Home Page (Form) ---
+# ROUTE 1: Home Page (Form)
 @app.route('/')
 def index():
-    # We still need the list of stations for the dropdown
+    # We need the list of stations for the dropdown
     stations = sorted(list(graph.adj.keys()))
     return render_template('index.html', stations=stations)
 
-# --- ROUTE 2: Results Page (Processing) ---
+# ROUTE 2: Results Page (Processing)
 @app.route('/result', methods=['POST'])
 def result():
-    # Get data from the HTML Form
+    # Getting data from the HTML Form
     source = request.form.get('source')
     destination = request.form.get('destination')
 
@@ -34,7 +34,7 @@ def result():
     if not route_data:
         return render_template('index.html', error="No route found!", stations=sorted(list(graph.adj.keys())))
 
-    # Render the Result Page with the data
+    # Rendering the Result Page with the data
     return render_template('result.html', data=route_data, source=source, destination=destination)
 
 if __name__ == '__main__':
